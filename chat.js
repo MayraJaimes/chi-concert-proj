1102b3d520a0fd93e3d685b0542a1fa80c054484
Mayra Jaimes <mtj26@georgetown.edu>
2018-01-17 20:29:48 -0600

    trying again to link database

--- chat.js
+++ chat.js
@@ -1,3 +1,15 @@
+var config = {
+    apiKey: "AIzaSyCXjft2kReyOPJVDnJci8SvwLzS9DjsOL0",
+    authDomain: "class-concert-project.firebaseapp.com",
+    databaseURL: "https://class-concert-project.firebaseio.com",
+    projectId: "class-concert-project",
+    storageBucket: "class-concert-project.appspot.com",
+    messagingSenderId: "623152189871"
+};
+ 
+firebase.initializeApp(config);
+
+
  var database = firebase.database();
 
  var userName = "";
