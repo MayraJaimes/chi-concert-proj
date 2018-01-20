5ded6ce23e6c260974383ab9bc54e31842eaf610
Ariana <ariana.dv4@gmail.com>
2018-01-20 10:46:45 -0600

    Added Login and Navbar

--- assets/scripts/homepage.js
+++ assets/scripts/homepage.js
@@ -1,17 +1,4 @@
-// Initialize Firebase
-var config = {
-apiKey: "AIzaSyDIlnbkpqY2Eg-QNtpjUBcse4t5kpX8ICU",
-authDomain: "chicago-concert-project.firebaseapp.com",
-databaseURL: "https://chicago-concert-project.firebaseio.com",
-projectId: "chicago-concert-project",
-storageBucket: "chicago-concert-project.appspot.com",
-messagingSenderId: "655657671055"
-};
-firebase.initializeApp(config);
 
-var database = firebase.database();
-
-//venue variables/ info that we want to add to each object;
 var name;
 var address;
 var website;
