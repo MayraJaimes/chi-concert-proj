3ccbc370a6cf092c8bb189c18e937eefc626d8d7
Matthew Murnighan <mattmurn@gmail.com>
2018-01-23 16:37:06 -0600

    completed changes to convertedHomepage.js

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,18 +1,19 @@
 console.log("linked");
 
-var city = $("#search");
+var city = $("#search").text();
 
  var upcoming = `http://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json?apikey={your_api_key}`
 
-city.text("Hello");
+city ="Chicago";
 
 var citySearch = `http://api.songkick.com/api/3.0/search/locations.json?query=${city}&apikey=awz1NrZkcMbHwia9`;
 var eventSearch = `http://api.songkick.com/api/3.0/events.json?apikey=awz1NrZkcMbHwia9`;
 $.ajax({
-    url: eventSearch,
+    url: citySearch,
     method: "GET"
   }).done(function(response) {
 
   	console.log(response);
 
   });
+
