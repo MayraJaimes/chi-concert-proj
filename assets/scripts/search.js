58dac271b8db796366df37c7fd0bf4f535b35dd5
Matthew Murnighan <mattmurn@gmail.com>
2018-01-24 18:07:11 -0600

    junk

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -4,9 +4,9 @@ var city = $("#search").text();
 
  var upcoming = `http://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json?apikey={your_api_key}`
 
-city ="Chicago";
+city ="aragon";
 
-var citySearch = `http://api.songkick.com/api/3.0/search/locations.json?query=${city}&apikey=awz1NrZkcMbHwia9`;
+var citySearch = `http://api.songkick.com/api/3.0/search/venues.json?query=${city}&apikey=awz1NrZkcMbHwia9`;
 var eventSearch = `http://api.songkick.com/api/3.0/events.json?apikey=awz1NrZkcMbHwia9`;
 $.ajax({
     url: citySearch,
